75d8e3f12a2e407ef2bd2c3ecb67f8f2d4790478
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2019-09-17 09:41:36 +0200

    new route rooms

--- Frontend/sensors/src/routes.js
+++ Frontend/sensors/src/routes.js
@@ -2,6 +2,7 @@ import Login from './components/auth/Login'
 import Dashboard from './components/backoffice/Dashboard'
 import Logout from './components/auth/Logout'
 import Profile from './components/backoffice/Profile'
+import Room from './components/backoffice/Room'
 
 const routes = [
   {
@@ -24,6 +25,14 @@ const routes = [
     path: '/profile',
     name: 'profile',
     component: Profile,
+    meta: {
+      requiresAuth: true,
+    }
+  },
+  {
+    path: '/rooms',
+    name: 'rooms',
+    component: Room,
     meta: {
       requiresAuth: true,
     }
